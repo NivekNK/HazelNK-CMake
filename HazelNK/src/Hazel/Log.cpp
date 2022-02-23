12836af966f8012908f6df2e29f5f6431decbe65
Kevin Rojas <juegosdelhambre25@outlook.com>
2022-02-23 18:03:24 -0300

    Trace Log change color to green

--- HazelNK/src/Hazel/Log.cpp
+++ HazelNK/src/Hazel/Log.cpp
@@ -17,14 +17,14 @@ namespace nk
 		s_CoreLogger->set_level(spdlog::level::trace);
 		const auto coreLoggerSink = dynamic_cast<spdlog::sinks::stdout_color_sink_mt*>(s_CoreLogger->sinks().back().get());
 #ifdef NK_PLATFORM_WINDOWS
-		coreLoggerSink->set_color(spdlog::level::trace, FOREGROUND_GREEN | FOREGROUND_BLUE);
+		coreLoggerSink->set_color(spdlog::level::trace, FOREGROUND_GREEN);
 		coreLoggerSink->set_color(spdlog::level::info, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 #endif
 		s_ClientLogger = spdlog::stderr_color_mt("APP");
 		s_ClientLogger->set_level(spdlog::level::trace);
 		const auto clientLoggerSink = dynamic_cast<spdlog::sinks::stderr_color_sink_mt*>(s_ClientLogger->sinks().back().get());
 #ifdef NK_PLATFORM_WINDOWS
-		clientLoggerSink->set_color(spdlog::level::trace, FOREGROUND_GREEN | FOREGROUND_BLUE);
+		clientLoggerSink->set_color(spdlog::level::trace, FOREGROUND_GREEN);
 		clientLoggerSink->set_color(spdlog::level::info, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 #endif
 	}
